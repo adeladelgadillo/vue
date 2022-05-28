c6e50709371fb99ec8daadca3ddec7d72fe338aa
unknown <adeladelgadillo123@gmail.com>
2022-05-27 22:36:41 -0300

    la lista(?

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,30 +1,68 @@
 <template>
   <div>
     <h1>{{msg}}</h1> 
-    <ol>
-         <li v-for="product in products" :key="product.id">{{ product.name }}
-        <img :src="product.image" /></li>
-     </ol>
+      <li v-for="product in products" :key="product.id">
+      
+    <img src="@/assets/polar.jpg" width="100" height="100" />
+      {{ product.name }}
+      </li>
+  
+
     </div>
 </template>
 
 <script>
-//import { mapState } from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   name: 'List',
   computed: {
-  //...mapState(['this.products']),
-    products() 
+  ...mapState(['products']),
+   /* products() 
     {
       return this.$store.state.products;
-    }
+    }*/
   },
   data () {
     return {
       msg: 'Ropa'
     }
   },
-}
 
+}
+/*    <ol>
+         <li v-for="product in products" :key="product.id">
+       <img src="@/assets/polar.jpg" width="100" height="100" />
+      {{ product.name }}
+      </li>
+     </ol>*/ 
 </script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ol {  
+  list-style-type: none;
+  text-align: center;
+}  
+
+
+li {  
+   padding:20px;
+   text-align: center;
+   
+}  
+
+div {  
+  display:flex;
+  justify-content: space-around;
+  align-items:center;
+  flex-direction: column;
+}  
+
+</style>
+
+
+
+
+
